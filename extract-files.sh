e6ba07ee1eeb61cad3830bdb0791b1e915360fc1
LuK1337 <priv.luk@gmail.com>
2024-09-08 18:43:59 +0200

    sm8150-common: Use `FIX_XML` instead of blob_fixup hook
    Change-Id: Ie4fdc07b0a3fb05acbd868b73d4b81710f8d6a22
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -72,10 +72,6 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             "${PATCHELF}" --replace-needed "libstagefright_foundation.so" "libstagefright_foundation-v33.so" "${2}"
             ;;
-        vendor/etc/data/dsi_config.xml | vendor/etc/data/netmgr_config.xml | vendor/etc/vintf/manifest/manifest_vendor.dolby.hardware.dms.xml)
-            [ "$2" = "" ] && return 0
-            fix_xml "${2}"
-            ;;
         vendor/lib64/libwvhidl.so | vendor/lib64/mediadrm/libwvdrmengine.so)
             [ "$2" = "" ] && return 0
             grep -q "libcrypto_shim.so" "${2}" || "${PATCHELF}" --add-needed "libcrypto_shim.so" "${2}"
