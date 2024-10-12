31927661ff17a38db62254764a4445d33d0c89e9
LuK1337 <priv.luk@gmail.com>
2024-10-12 17:42:02 +0200

    sm8150-common: Shim WFD with libgui_shim/libinput_shim
    Change-Id: I42bf06bc6b19942303031ef0147dafa6df127fe5
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -88,6 +88,15 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             grep -q "madvise: 1" "${2}" || echo -e "\nmadvise: 1" >> "${2}"
             ;;
+        system_ext/lib/libwfdmmsrc_system.so)
+            [ "$2" = "" ] && return 0
+            grep -q "libgui_shim.so" "${2}" || "${PATCHELF}" --add-needed "libgui_shim.so" "${2}"
+            ;;
+        system_ext/lib64/libwfdnative.so)
+            [ "$2" = "" ] && return 0
+            "${PATCHELF}" --replace-needed "android.hidl.base@1.0.so" "libhidlbase.so" "${2}"
+            grep -q "libinput_shim.so" "${2}" || "${PATCHELF}" --add-needed "libinput_shim.so" "${2}"
+            ;;
         *)
             return 1
             ;;
